aab97805d7840b2d71f82c0cc7fd283f993cac5a
Steve Gattuso <stevenleeg@gmail.com>
2013-10-02 00:10:29 -0400

    Everything is now stored in the database

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -1,13 +1,28 @@
+from sqlalchemy.orm import sessionmaker
 from scrapy.exceptions import DropItem
+from model import Dish, db_connect, create_table
 
 class PostgresPipeline(object):
     def open_spider(self, spider):
-        # TODO: DB stuff
-        return
+        engine = db_connect()
+        create_table(engine)
+        self.Session = sessionmaker(bind = engine)
 
     def process_item(self, item, spider):
         print "%s::%s::%s::%s" % (item['location'], item['station'], item['date'], item['title'])
+        session = self.Session()
+        item = Dish(**item)
+        item.generate_id()
 
-    def close_spider(self, spider):
-        # TODO: Moar db stuff
-        return
+        # See if it already exists
+        count = session.query(Dish).filter(Dish.id == item.id).count()
+        if count != 0:
+            print "Skipping!"
+            session.close()
+            return item
+
+        session.add(item)
+        session.commit()
+        session.close()
+
+        return item
