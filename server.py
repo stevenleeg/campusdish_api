9f72adb02db4a5a1ce6e7c3ceaaf58b29f09f29c
Steve Gattuso <stevenleeg@gmail.com>
2013-10-02 11:20:54 -0400

    Gutting some of the postgres stuff

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect
+from flask.ext.restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route("/")
 def index():
-    return "campusdish api"
+    return redirect("https://github.com/stevenleeg/campusdish_api/blob/master/README.md")
+
+class Location(Resource):
+    def get(self, location):
+        return { "hello": location }
+
+api.add_resource(Location, "/v1/location/<string:location>")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
