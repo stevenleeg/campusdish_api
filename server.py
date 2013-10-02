44a05c803f0d0405fb2d44eb44f46e2b082240da
Steve Gattuso <stevenleeg@gmail.com>
2013-10-02 12:06:20 -0400

    Starting on actual API

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, g
 from flask.ext.restful import Resource, Api
+from pymongo import MongoClient
+import os, datetime
 
 app = Flask(__name__)
 api = Api(app)
+
+@app.before_request
+def db_connect():
+    g.conn = MongoClient(os.environ['DB_HOST'], int(os.environ['DB_PORT']))
+    g.db = g.conn['cd_api']
 
 @app.route("/")
 def index():
@@ -10,7 +17,29 @@ def index():
 
 class Location(Resource):
     def get(self, location):
-        return { "hello": location }
+        coll = g.db['dishes']
+
+        current = datetime.date.today()
+        week_begin = current - datetime.timedelta(days = (current.weekday() + 1 % 6))
+        week_begin = datetime.datetime(week_begin.year, week_begin.month, week_begin.day)
+        week_end = week_begin + datetime.timedelta(days = 7)
+
+        meals = coll.find({ 
+            "location": location, 
+            "date": { "$gte": week_begin, "$lt": week_end }
+        }).sort("station")
+
+        stations = {}
+        for meal in meals:
+            if meal['station'] not in stations:
+                stations[meal['station']] = []
+            
+            stations[meal['station']].append({ 
+                "title": meal['title'],
+                "date": str(meal['date'])
+            })
+
+        return { "location": stations }
 
 api.add_resource(Location, "/v1/location/<string:location>")
 
