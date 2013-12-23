850e280b2784ca62ed3a55b0eca815887ea5c5e8
Steve Gattuso <stevenleeg@gmail.com>
2013-12-22 20:34:29 -0500

    Changing endpoint versions

--- campusdish_api/routes.py
+++ campusdish_api/routes.py
@@ -2,6 +2,6 @@ from campusdish_api import api
 import campusdish_api.resources as resources
 
 
-api.add_resource(resources.DiningHallResource, "/v1/dining_hall/<string:location>/<string:meal>")
-api.add_resource(resources.DiningHallsResource, "/v1/dining_hall")
-api.add_resource(resources.MealsResource, "/v1/meal")
+api.add_resource(resources.DiningHallResource, "/v0/dining_hall/<string:location>/<string:meal>")
+api.add_resource(resources.DiningHallsResource, "/v0/dining_hall")
+api.add_resource(resources.MealsResource, "/v0/meal")
